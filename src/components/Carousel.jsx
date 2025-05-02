5ea226116a20ebe97811171dd556cf13ddd42879
Vedant Elwatkar <vedanthelwatkar@gmail.com>
2025-05-02 20:50:00 +0530

    height 100vh in mobile

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -186,7 +186,7 @@ const Carousel = ({
         )}
       </AnimatePresence>
 
-      <div className="h-[90vh] pt-20 sm:pt-0 md:h-[80vh] relative flex items-center">
+      <div className="h-[100vh] pt-20 sm:pt-0 md:h-[80vh] relative flex items-center">
         <div className="w-full px-6 relative z-20 flex flex-col md:flex-row items-center">
           <AnimatePresence
             initial={false}
